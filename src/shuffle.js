8bfde36cad2bf81d82b930a087465f327e9d437a
philip ross <philip@satchel.scot>
2023-09-29 12:43:05 +0100

    feat: update shuffle to use spread

--- src/shuffle.js
+++ src/shuffle.js
@@ -7,15 +7,19 @@ function rng() {
 // Use Fisher–Yates shuffle http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 // see also http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 // to get random permutation from a finite set.
-function getRandomSubarray(arr, size) {
+/**
+ * @param {any} array
+ * @param {number} size
+ */
+function getRandomSubarray(array, size) {
   const shuffled = [];
   let index = 0;
   let rand;
-  arr.forEach(value => {
+  for (const value of array) {
     rand = Math.floor((index++ + 1) * _rng());
     shuffled[index - 1] = shuffled[rand];
     shuffled[rand] = value;
-  });
+  }
 
   // return the part we want
   return size ? shuffled.slice(0, size) : shuffled;
@@ -24,16 +28,20 @@ function getRandomSubarray(arr, size) {
 // Use Fisher–Yates shuffle http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 // see also http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 // to get random permutation from a finite set.
-function getRandomSubarrayAlt(arr, size) {
-  const shuffled = arr.slice();
-  let count = arr.length;
+/**
+ * @param {string | any[]} array
+ * @param {number} size
+ */
+function getRandomSubarrayAlt(array, size) {
+  const shuffled = [...array];
+  let count = array.length;
   let rand;
-  let temp;
+  let temporary;
   while (count) {
-    rand = (_rng() * count--) | 0;
-    temp = arr[count];
-    arr[count] = arr[rand];
-    arr[rand] = temp;
+    rand = Math.trunc(_rng() * count--);
+    temporary = array[count];
+    array[String(count)] = array[rand];
+    array[String(rand)] = temporary;
   }
 
   // return the part we want
